45870f333fdf22bcaa21b755f14dcd5c0c32ee70
tjel <tomek.jelinski@gmail.com>
2016-10-24 01:18:40 +0200

    Add triangle-test.cpp.

---
+++ triangle-test.cpp
@@ -0,0 +1,19 @@
+# include <iostream>
+
+using namespace std;
+
+int main( ) 
+{ 
+    double a; 
+    double b;
+    double c;
+    cout<< "Enter a"; 
+    cin>>a;
+    cout<< "Enter b"; 
+    cin>>b;
+    cout<< "Enter c"; 
+    cin>>c;
+ 
+    cout<< "Perimeter of the triangle is: "<< a+b+c; 
+    return 0;
+} 
